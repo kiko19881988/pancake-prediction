eb36e9a95f2dd9a55ada8cfbfd8e2fd18b2eec1e
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-02-20 18:52:11 -0500

    freezing package versions

--- ui/history.py
+++ ui/history.py
@@ -11,7 +11,13 @@ def get_history(_psp, current_epoch, back_in_time=100):
     df_history_round = pd.DataFrame(columns=round_columns)
     for i in range(start_epoch_history, current_epoch - 1):
         df_round = _psp.get_round(i)
-        df_history_round = pd.concat([df_history_round, df_round])
+
+        if df_round.empty:
+            df_history_round = df_history_round.copy()
+        if df_history_round.empty:
+            df_history_round = df_history_round.copy()
+        if not df_round.empty and not df_history_round.empty:
+            df_history_round = pd.concat([df_history_round, df_round])
 
     df_history_round = df_history_round.sort_values('epoch', ascending=False)
     df_history_round = df_history_round.reset_index(drop=True)
